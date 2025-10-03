8fc48b009a1b1972c2115de25af8d40960c36bd1
Uddeshya <vddeshya10d@gmail.com>
2025-10-03 12:32:48 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'  // Updated for GitHub Pages
+  base: '/Resume/',  // Update base URL to match repository name
 })
