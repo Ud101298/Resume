9a8baa2c15105cb1c905c54b90222e0b20455f93
Uddeshya <vddeshya10d@gmail.com>
2025-10-03 12:07:47 +0530

    Add Github Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Resume/'  // Replace 'Resume' with your repository
+  base: './'  // Updated for GitHub Pages
 })
