56f6a9c964b0fe0a22313a5cc4f95140b19c53ba
Uddeshya <vddeshya10d@gmail.com>
2025-10-03 12:36:15 +0530

    change in vite route

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Resume/',  // Update base URL to match repository name
+  base: '/Resume/', // 👈 This is the key fix
 })
